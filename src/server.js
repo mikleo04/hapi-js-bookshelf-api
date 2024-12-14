3109c7532779b9514f55a2530b7cddc5718cbee2
hafid.witra <hafid.witra@jatis.com>
2024-12-14 07:10:35 +0700

    feature: instance server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      }
+    }
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
+
